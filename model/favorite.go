63450e617e6652aba49006cc08567e56305e017a
GuanJunZhang <2062936335@qq.com>
2022-03-29 16:20:06 +0800

    refactor(我的收藏模块) 保证收藏接口幂等性

--- model/favorite.go
+++ model/favorite.go
@@ -1,19 +1,32 @@
-package model 
+package model
 
 import (
+	"errors"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 //收藏普法问题
 func FavoriteAdd(favorite *Favorite) error {
-	_, err := Db.InsertOne(favorite)
-	return err
+	has, err := Db.Exist(&Favorite{
+		UserId:  favorite.UserId,
+		IssueId: favorite.IssueId,
+	})
+	if err != nil {
+		return err
+	}
+	if has {
+		return errors.New("该普法问题已被收藏，请勿重复收藏！")
+	} else {
+		_, err = Db.InsertOne(favorite)
+		return err
+	}
 }
 
 //取消收藏普法问题
 func FavoriteCancel(uid int, issueId int) error {
 	_, err := Db.Delete(&Favorite{
-		UserId: uid,
+		UserId:  uid,
 		IssueId: issueId,
 	})
 	return err
@@ -22,7 +35,7 @@ func FavoriteCancel(uid int, issueId int) error {
 //问题是否收藏
 func IssueIsFavorite(uid int, issueId int) (bool, error) {
 	has, err := Db.Exist(&Favorite{
-		UserId: uid,
+		UserId:  uid,
 		IssueId: issueId,
 	})
 	return has, err
@@ -32,8 +45,8 @@ func IssueIsFavorite(uid int, issueId int) (bool, error) {
 func FavoriteList(uid int) ([]LegalIssue, error) {
 	legalIssueList := []LegalIssue{}
 	err := Db.Table("legal_issue").
-	Join("INNER", "favorite", "favorite.issue_id = legal_issue.id").
-	Where("favorite.user_id = ?", uid).
-	Find(&legalIssueList)
+		Join("INNER", "favorite", "favorite.issue_id = legal_issue.id").
+		Where("favorite.user_id = ?", uid).
+		Find(&legalIssueList)
 	return legalIssueList, err
 }
