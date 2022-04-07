8725fd25c215b9157ad7feab83295b1ed2f588c8
GuanJunZhang <2062936335@qq.com>
2022-04-07 18:21:26 +0800

    参数校验

--- model/structs.go
+++ model/structs.go
@@ -14,10 +14,10 @@ type Category struct {
 
 type Consultation struct {
 	Id            int       `xorm:"not null pk autoincr UNSIGNED INT(10)" json:"id"`
-	Question      string    `xorm:"not null comment('咨询问题') TEXT" json:"question"`
+	Question      string    `xorm:"not null comment('咨询问题') TEXT" json:"question" validate:"required"`
 	Imgs          string    `xorm:"comment('描述图片') TEXT" json:"imgs"`
-	ConsultantUid int       `xorm:"not null comment('咨询人uid') index UNSIGNED INT(10)" json:"consultant_uid"`
-	Status        string    `xorm:"not null default '处理中' comment('处理中，待人工咨询、人工咨询中、已完成') VARCHAR(10)" json:"status"`
+	ConsultantUid int       `xorm:"not null comment('咨询人uid') index UNSIGNED INT(10)" json:"consultant_uid" validate:"required"`
+	Status        string    `xorm:"not null default '处理中' comment('处理中，待人工咨询、人工咨询中、已完成') VARCHAR(10)" json:"status" validate:"required,oneof=处理中 待人工咨询 人工咨询中 已完成"`
 	CreateTime    int       `xorm:"INT(10)" json:"create_time"`
 	UpdateTime    time.Time `xorm:"not null updated DateTime" json:"update_time"`
 }
@@ -26,8 +26,8 @@ type ConsultationRecord struct {
 	Id              int       `xorm:"not null pk autoincr UNSIGNED INT(10)" json:"id"`
 	ConsultationId  int       `xorm:"not null comment('咨询id') index UNSIGNED INT(10)" json:"consultation_id"`
 	CommunicatorUid int       `xorm:"not null comment('沟通人uid') UNSIGNED INT(10)" json:"communicator_uid"`
-	Type            string    `xorm:"not null comment('回复类型，answer,query') VARCHAR(20)" json:"type"`
-	Content         string    `xorm:"comment('回复内容') LONGTEXT" json:"content"`
+	Type            string    `xorm:"not null comment('回复类型，answer,query') VARCHAR(20)" json:"type" validate:"required,oneof=answer query"`
+	Content         string    `xorm:"comment('回复内容') LONGTEXT" json:"content" validate:"required"`
 	CreateTime      int       `xorm:"INT(10)" json:"create_time"`
 	UpdateTime      time.Time `xorm:"not null updated DateTime" json:"update_time"`
 }
@@ -66,8 +66,8 @@ type User struct {
 type InfringementDetection struct {
 	Id           int       `xorm:"not null pk autoincr UNSIGNED INT(10)" json:"id"`
 	CreatorUid   int       `xorm:"not null comment('创建人id') index UNSIGNED INT(10)" json:"creator_uid"`
-	Name         string    `xorm:"not null comment('姓名') VARCHAR(16)" json:"name"`
-	Phone        string    `xorm:"not null comment('电话号码') CHAR(20)" json:"phone"`
+	Name         string    `xorm:"not null comment('姓名') VARCHAR(16)" json:"name" validate:"required"`
+	Phone        string    `xorm:"not null comment('电话号码') CHAR(20)" json:"phone" validate:"required"`
 	Organization string    `xorm:"comment('组织机构') VARCHAR(60)" json:"organization"`
 	Description  string    `xorm:"comment('侵权监测描述') TEXT" json:"description"`
 	Resume       string    `xorm:"comment('权利概要') TEXT" json:"resume"`
@@ -77,9 +77,9 @@ type InfringementDetection struct {
 
 type DetectionReturnVisit struct {
 	Id              int       `xorm:"not null pk autoincr UNSIGNED INT(10)" json:"id"`
-	CreatorUid      int       `xorm:"not null comment('创建人id') index UNSIGNED INT(10)" json:"creator_uid"`
-	DetectionId     int       `xorm:"not null comment('监测id') UNSIGNED INT(10)" json:"detection_id"`
-	Classification  string    `xorm:"not null comment('类别') VARCHAR(10)" json:"classification"`
+	CreatorUid      int       `xorm:"not null comment('创建人id') index UNSIGNED INT(10)" json:"creator_uid" validate:"required"`
+	DetectionId     int       `xorm:"not null comment('监测id') UNSIGNED INT(10)" json:"detection_id" validate:"required"`
+	Classification  string    `xorm:"not null comment('类别') VARCHAR(10)" json:"classification" validate:"required,oneof=未回访 有合作意向 无合作意向 已合作"`
 	CustomerAddress string    `xorm:"not null comment('客户地址') VARCHAR(50)" json:"customer_address"`
 	Remark          string    `xorm:"comment('备注') TEXT" json:"remark"`
 	CreateTime      int       `xorm:"INT(10)" json:"create_time"`
@@ -88,8 +88,8 @@ type DetectionReturnVisit struct {
 type RightsProtection struct {
 	Id           int       `xorm:"not null pk autoincr UNSIGNED INT(10)" json:"id"`
 	CreatorUid   int       `xorm:"not null comment('创建人id') index UNSIGNED INT(10)" json:"creator_uid"`
-	Name         string    `xorm:"not null comment('姓名') VARCHAR(16)" json:"name"`
-	Phone        string    `xorm:"not null comment('电话号码') CHAR(20)" json:"phone"`
+	Name         string    `xorm:"not null comment('姓名') VARCHAR(16)" json:"name" validate:"required"`
+	Phone        string    `xorm:"not null comment('电话号码') CHAR(20)" json:"phone" validate:"required"`
 	Organization string    `xorm:"comment('组织结构') VARCHAR(60)" json:"organization"`
 	Description  string    `xorm:"comment('维权意向描述') TEXT" json:"description"`
 	Resume       string    `xorm:"comment('权利概要') TEXT" json:"resume"`
@@ -99,9 +99,9 @@ type RightsProtection struct {
 
 type ProtectionReturnVisit struct {
 	Id              int       `xorm:"not null pk autoincr UNSIGNED INT(10)" json:"id"`
-	CreatorUid      int       `xorm:"not null comment('创建人id') index UNSIGNED INT(10)" json:"creator_uid"`
-	ProtectionId    int       `xorm:"not null comment('维权id') UNSIGNED INT(10)" json:"protection_id"`
-	Classification  string    `xorm:"not null comment('类别') VARCHAR(10)" json:"classification"`
+	CreatorUid      int       `xorm:"not null comment('创建人id') index UNSIGNED INT(10)" json:"creator_uid" validate:"required"`
+	ProtectionId    int       `xorm:"not null comment('维权id') UNSIGNED INT(10)" json:"protection_id" validate:"required"`
+	Classification  string    `xorm:"not null comment('类别') VARCHAR(10)" json:"classification" validate:"required,oneof=未回访 有合作意向 无合作意向 已合作"`
 	CustomerAddress string    `xorm:"not null comment('客户地址') VARCHAR(50)" json:"customer_address"`
 	Remark          string    `xorm:"comment('备注') TEXT" json:"remark"`
 	CreateTime      int       `xorm:"INT(10)" json:"create_time"`
